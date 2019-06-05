15d1c0a21b7cb23eedb6fcea2050397dea76c8d2
Amila Welihinda <amilajack@gmail.com>
2019-06-04 22:30:07 -0700

    Updates

---
+++ precache-manifest.a640d629e048ccc55bc2dd882605f57a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfc7781acf59ff3cae9cf4f307335b48",
+    "url": "/amilajack/webvr-museum/index.html"
+  },
+  {
+    "revision": "33648d02decd89b6152c",
+    "url": "/amilajack/webvr-museum/static/js/2.b7aa9a1c.chunk.js"
+  },
+  {
+    "revision": "8dc610ad6d06021df132",
+    "url": "/amilajack/webvr-museum/static/js/main.68a17a18.chunk.js"
+  },
+  {
+    "revision": "c3371fc291022ca2eb4d",
+    "url": "/amilajack/webvr-museum/static/js/runtime~main.ab0dcc7f.js"
+  }
+]);
